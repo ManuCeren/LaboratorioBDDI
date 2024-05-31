7ba0a9e18bf25c9d61f5487cd25472913ef927a0
ManuCeren <ca22i04002@usonsonate.edu.sv>
2024-05-30 21:48:35 -0600

    Guia 4 Terminada

--- GUIA04/query.sql
+++ GUIA04/query.sql
@@ -1,39 +1,12 @@
--- CONSULTAS UTILIZANDO INNER JOIN --
-
--- CONSULTANDO 01: 
-		-- Mostrar una lista de los medicamentos que se tienen a la venta, 
-		-- con los detalles del tipo de producto, presentación y proveedor (laboratorio)
-SELECT 
-    med.nombreMedicamento, 
-    cat.nombreCategoria,
-    mar.nombreMarca,
-    pre.nombrePresentacion,
-    lab.nombreLaboratorio
-FROM medicamentos med
-    INNER JOIN categorias cat ON med.idCategoria = cat.idCategoria
-    INNER JOIN marcas mar ON med.idMarca = mar.idMarca
-    INNER JOIN presentacionMedicamento pre ON med.idPresentacionMedicamento = pre.idPresentacionMedicamento
-    INNER JOIN detalleCompra dc ON med.idMedicamento = dc.idMedicamento
-    INNER JOIN compras com ON dc.idCompra = com.idCompra
-    INNER JOIN laboratorios lab ON com.idLaboratorio = lab.idLaboratorio;
-    
--- CONSULTA 02: 
-	-- Mostrar una lista de las ventas realizadas, con los detalles del cliente que compró, 
-    -- el vendedor, y el monto de la venta incluyendo impuestos fiscales como el IVA y
-    -- crédito fiscal (si está registrando este último)
-SELECT
-  ven.fechaHoraVenta AS Fecha_Venta,
-  CONCAT(nc.nombresCliente, ' ', nc.apellidosCliente) AS Cliente,
-  CONCAT(emp.nombresEmpleado, ' ', emp.apellidosEmpleado) AS Empleado,
-  dv.cantidadVendida,
-  dv.precioUnitario,
-  (dv.cantidadVendida * dv.precioUnitario) AS SubTotal,
-  fv.ivaFacturaVenta AS IVA,
-  fv.creditoFiscalVenta AS Credito_Fiscal,
-  (dv.cantidadVendida * dv.precioUnitario) + fv.ivaFacturaVenta AS Total
-FROM ventas ven
-	INNER JOIN detalleVenta dv ON ven.idVenta = dv.idVenta
-	INNER JOIN medicamentos med ON dv.idMedicamento = med.idMedicamento
-	INNER JOIN clientes nc ON ven.idCliente = nc.idCliente
-	INNER JOIN empleados emp ON ven.idEmpleado = emp.idEmpleado
-	INNER JOIN facturaVenta fv ON dv.idFacturaVenta = fv.idFacturaVenta;
+						-- 	CONSULTAS --
+-- CONSULTA 1 : SELECCIONA TODOS LOS CLIENTES QUE SE LLAMEN JUAN --
+SELECT * FROM clientes WHERE nombresCliente = 'Juan'; -- 1
+-- CONSULTA 2 : SELECCIONA TODOS LOS PRODUCTOS CUYO PRECIO DE VENTA ES MAYOR A $10.00 --
+SELECT * FROM medicamentos WHERE precioVenta > 10; -- 2
+-- ACTUALIZACIÓN 1 : CAMBIA EL NUMERO DE TELEFONO DEL EMPLEADO CON ID=1 --
+UPDATE empleados SET telefonoEmpleado = '7807-2350' WHERE idEmpleado = 1; -- 3
+-- ACTUALIZACIÓN 2: CAMBIA EL CORREO DEL CLIENTE CON ID 1 --
+UPDATE clientes SET correoCliente = 'juanperez5023@example.com' WHERE idCliente = 1; -- 4
+-- ELIMINACIÓN: ELIMINAR TODOS LOS REGISTROS EN LA TABLA DETALLE DE VENTA QUE ESTAN RELACIONADOS CON LOS REGISTROS DE VENTAS DEL CLIENTE CON ID=1
+DELETE FROM detalleventa WHERE idVenta IN (SELECT idVenta FROM ventas WHERE idCliente = '1');
+DELETE FROM ventas WHERE idCliente = '1';
